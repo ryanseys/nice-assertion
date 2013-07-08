6081ed9039453f901736f6ef3a5c9d75c2f66187
Ryan Seys <ryan@ryanseys.com>
2013-07-07 17:19:08 -0700

    Change labels

--- assertion.py
+++ assertion.py
@@ -5,7 +5,7 @@ import json
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-TITLE = "Nice Assertion!"
+TITLE = "Nice assertion! ;)"
 NOT_VALID_ASSERTION = "Invalid assertion! :("
 VALID_ASSERTION = "Success! Valid assertion! :)"
 
